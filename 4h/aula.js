84c3b535fecc0c861b57f28ec0d67fc31a105a9a
Rubens Freitas <rubens.suporte@gmail.com>
2021-04-14 01:26:53 -0300

    01:26

---
+++ 4h/aula.js
@@ -0,0 +1,2 @@
+console.log('Rubens')
+console.log('Bem vindo ao curso!')
---
+++ 4h/scripts.js
@@ -0,0 +1,10 @@
+let idade = 5;
+console.log(idade);
+let altura = 180;
+console.log(altura);
+
+
+let peso = 110;
+console.log(peso);
+let cabelo = castanho;
+console.log(cabelo);
