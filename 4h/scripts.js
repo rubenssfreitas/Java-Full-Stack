6154453b2b9eb0a10221e6f0357cf388e39fee52
Rubens Freitas <rubens.suporte@gmail.com>
2021-04-14 01:58:54 -0300

    01:58

--- 4h/scripts.js
+++ 4h/scripts.js
@@ -1,10 +1,19 @@
-let idade = 5;
-console.log(idade);
-let altura = 180;
-console.log(altura);
+let nome = 'Rubens';
+let idade = 45;
+let estaAprovado = true;
+let sobrenome = undefined;
 
+let pessoa = {
+    nome: 'Rubens',
+    idade: 45,
+    estaAprovado: true,
+    sobrenome: 'da Silva'
+};
 
-let peso = 110;
-console.log(peso);
-let cabelo = castanho;
-console.log(cabelo);
+console.log (pessoa);
+
+//arrays
+
+let nomeColega = ['Diego',23,'Ceará',36];
+console.log(nomeColega.length);
+console.log(nomeColega[1]);
